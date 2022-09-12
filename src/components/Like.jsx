8b7a43ee70ab4464d05013f1d7b7ece62e05e082
Saeedmaldosary <saeedmaldosary@gmail.com>
2022-09-12 16:48:12 +0300

    Add like component

---
+++ src/components/Like.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+
+class Like extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <FontAwesomeIcon
+          onClick={this.props.onClick}
+          icon={this.props.liked == true ? faHeartSolid : faHeartRegular}
+        />
+      </div>
+    );
+  }
+}
+
+export default Like;
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies, deleteMovie } from "../services/fakeMovieService";
+import Like from "./Like";
 
 class Movies extends Component {
   state = {
@@ -17,6 +18,7 @@ class Movies extends Component {
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
           </thead>
           {this.getMoviesList()}
@@ -32,6 +34,19 @@ class Movies extends Component {
       : "There are no movies in the database.";
   };
 
+  handleLike = (movie) => {
+    var movies = this.state.movies;
+    const index = this.state.movies.indexOf(movie);
+    if (
+      this.state.movies[index].liked == undefined ||
+      this.state.movies[index].liked == false
+    )
+      this.state.movies[index].liked = true;
+    else this.state.movies[index].liked = false;
+
+    this.setState({ movies: movies });
+  };
+
   getMoviesList() {
     return (
       <tbody>
@@ -41,6 +56,12 @@ class Movies extends Component {
             <th>{movie.genre.name}</th>
             <th>{movie.numberInStock}</th>
             <th>{movie.dailyRentalRate}</th>
+            <th>
+              <Like
+                liked={movie.liked}
+                onClick={() => this.handleLike(movie)}
+              />
+            </th>
             <th>
               <button
                 onClick={() => this.deleteMovieList(movie._id)}
