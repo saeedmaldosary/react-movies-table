ddc1d7c0ced47afc47dfa26a1cc20061341958e0
Saeedmaldosary <saeedmaldosary@gmail.com>
2022-09-19 17:01:55 +0300

    Fix column index

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -3,38 +3,43 @@ import Like from "./like";
 
 class TableBody extends Component {
   render() {
-    var { data, columns, onLike, onDelete } = this.props;
+    var { data, columns } = this.props;
     return (
       <React.Fragment>
         {data.map((d) => (
-          <tr key={d._id}>
-            {columns.map((c) => this.renderCell(c, d))}
-            <td>
-              <Like liked={d.liked} onClick={() => onLike(d)} />
-            </td>
-            <td>
-              <button
-                onClick={() => onDelete(d._id)}
-                className="btn btn-danger btn-sm"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
+          <tr key={d._id}>{columns.map((c) => this.renderCell(c, d))}</tr>
         ))}
       </React.Fragment>
     );
   }
 
   renderCell = (column, data) => {
+    var { onLike, onDelete } = this.props;
+    var key = this.createKey(data, column);
+
     if (column.name.includes(".")) {
       return (
-        <td key={this.createKey(data, column)}>
-          {data[column.name.split(".")[0]][column.subName]}
+        <td key={key}>{data[column.name.split(".")[0]][column.subName]}</td>
+      );
+    } else if (column.name === "like") {
+      return (
+        <td key={key}>
+          <Like liked={data.liked} onClick={() => onLike(data)} />
+        </td>
+      );
+    } else if (column.name === "delete") {
+      return (
+        <td key={key}>
+          <button
+            onClick={() => onDelete(data._id)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
         </td>
       );
     } else {
-      return <td key={this.createKey(data, column)}>{data[column.name]}</td>;
+      return <td key={key}>{data[column.name]}</td>;
     }
   };
 
